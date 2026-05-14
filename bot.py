2aff38ee69513d08a604c0c60c1c0a7867abb0cd
axelboltnobanpls-cmyk <axelboltnobanpls@gmail.com>
2026-05-14 08:27:00 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ DATABASE_FILE = os.path.join(BASE_DIR, "users.db")
 CONFIG_FILE = os.path.join(BASE_DIR, "config.json")
 
 # ======================== НАСТРОЙКИ ========================
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "СТАРЫЙ_ТОКЕН_НА_СЛУЧАЙ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8675981214:AAHkjjcFAhSTisGVF15O7MR3Be7hUhVyasg")
 CHANNEL_USERNAME = "@copperS_shop"
 CHANNEL_LINK = "https://t.me/copperS_shop"
 GGSEL_LINK = "https://ggsel.net/sellers/132805517"
